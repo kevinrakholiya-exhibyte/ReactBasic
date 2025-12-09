eab946d3cd53cca44b0de79cfb7037ee1419b649
Kevin Rakholiya-Exhibyte <kevin.rakholiya@exhibytesolution.com>
2025-12-09 18:42:50 +0530

    Add User and Login page

--- ReactBasic/src/Pages/Home.jsx
+++ ReactBasic/src/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import CounterFunction from '../Component/CounterFunction'
+import User from '../Component/User'
+import UseState from '../Hooks/UseState'
 
 const Home = () => {
     return (
         <div>
             <h1>Home Page</h1>
             <CounterFunction />
+            <UseState/>
+            <User name="Kevin" age={21} uid={101}/>
         </div>
 
     )
